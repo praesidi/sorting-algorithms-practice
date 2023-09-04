6b3d26509275bbd90f003f5ef02bd4d31127d7be
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-09-04 20:21:37 +0300

    Add insertion sort algorithm implementation

---
+++ insertion-sort.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      if (arr[j] < arr[j - 1]) {
+        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+      }
+      j--;
+    }
+  }
+  return arr;
+}
